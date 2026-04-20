db0f3a59167de7a8bf9d598d72dcf8077325f997
alexgetahuntadese <alexgetahuntadese@gmail.com>
2026-04-20 07:45:56 +0300

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,6 +1,16 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   turbopack: {},
+
+  async redirects() {
+    return [
+      {
+        source: '/:path*',
+        destination: 'https://successdoor.vercel.app/:path*',
+        permanent: true,
+      },
+    ];
+  },
 };
 
 export default nextConfig;
